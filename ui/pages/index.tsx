e46b64498f23f2c75544deab477b43603e6f256c
hamzakh777 <hamzabitup@gmail.com>
2022-09-06 21:40:14 +0100

    chore: scaffol front-end

---
+++ ui/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Smart Contract Lottery</title>
+        <meta name="description" content="Smart Contract Lottery" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      asdf
+    </div>
+  )
+}
+
+export default Home
